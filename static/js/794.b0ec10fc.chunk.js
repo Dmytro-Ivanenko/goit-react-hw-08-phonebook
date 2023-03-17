65856a00e6c705fe80ac0b4828e91bbf3f7ca941
Dmytro-Ivanenko <107716921+Dmytro-Ivanenko@users.noreply.github.com>
2023-03-17 09:22:14 +0000

    Deploying to gh-pages from @ Dmytro-Ivanenko/goit-react-hw-08-phonebook@3295a19e261eec0ab28ecc5b0dfe4dd0b27cc9ef 🚀

---
+++ static/js/794.b0ec10fc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{1794:function(e,t,r){r.r(t);r(2791);var a=r(7689),u=r(3329);t.default=function(){return(0,u.jsx)(a.Fg,{to:"/"})}}}]);
+//# sourceMappingURL=794.b0ec10fc.chunk.js.map
